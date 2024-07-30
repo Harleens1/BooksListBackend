a4eeea47044b9d7d0674f21112b6d45971b1dd27
Harleens <harleens@mun.ca>
2024-07-29 19:58:34 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://harleens:h9vi9xHqsHc3V2Pj@bookstore.zmmhfye.mongodb.net/?retryWrites=true&w=majority';
+
+
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
